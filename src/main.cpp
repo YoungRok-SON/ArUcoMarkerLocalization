ab00bfc0fa180fabb556fc74233d83c2183691c4
YoungRok-SON <dudfhr3349@gmail.com>
2023-07-01 22:11:05 +0900

    Initialization.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/* 
+author      : YoungRok Son
+Date        : 2023-07-01
+Email       : dudfhr3349@gmail.com
+Description : This pakcage is for testing indoor UAV localization using ArUco marker of opencv. 
+*/
+
+
+#include "../include/utilities.hpp"
+
+
+int main()
+{
+    std::cout << "main is running..." << std::endl;
+    return 1;
+}
